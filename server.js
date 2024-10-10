94277de121133f8852cb787a5bb85bd1ae801481
cYbEr MaNiAc <32965165+ketanmahajan11@users.noreply.github.com>
2024-10-11 04:45:33 +0530

    Update server.js
    updated the backend url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://yourusername.github.io'  // Replace with your GitHub Pages URL
+  origin: 'https://nitroliftsupports.onrender.com'  // Replace with your GitHub Pages URL
 }));
 app.use(express.json());
 
